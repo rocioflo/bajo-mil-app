f0c81d20cb36cf5cef7a8d924b5bc95791ea8629
Rocio Flo <rocioflorencioytal@gmail.com>
2022-10-05 18:05:28 +0200

    Working call to api based on artist choice

--- src/App.js
+++ src/App.js
@@ -4,22 +4,70 @@ import { useEffect, useState } from 'react';
 import { useQuery } from '@tanstack/react-query';
 import axios from 'axios';
 
-function ArtistForm() {
+function ArtistForm({handleSelect}) {
   return <form className="first-form">
           <label className="first-form-label">
             Selecciona un artista que le hable a tu alma
-            <select>
-              <option>Taylor Swift</option>
-              <option>Dodie</option>
-              <option>Sammy Rae & The Friends</option>
+            <select className='first-form-select' onChange={handleSelect}>
+              <option value='06HL4z0CvFAxyc27GXpf02'>Taylor Swift</option>
+              <option value='21TinSsF5ytwsfdyz5VSVS'>Dodie</option>
+              <option value='3lFDsTyYNPQc8WzJExnQWn'>Sammy Rae & The Friends</option>
             </select>
           </label>
           <input type="submit" value="Dale" className="first-form-button" />
         </form>
 }
 
-function renderGenreForm() {
-  return 
+function GenreForm({handleRadio}) {
+  return <form className="second-form">
+  <label className="second-form-label">Selecciona un género</label>
+  <label className='genre-pop'>
+    Pop
+    <input
+      type="radio"
+      name="genre"
+      value="pop"
+      className='input-pop'
+      onChange={handleRadio}
+    />
+  </label>
+  <label className='genre-indie'>
+    Indie
+    <input
+      type="radio"
+      name="genre"
+      value="indie"
+      onChange={handleRadio}
+    />
+  </label>
+  <label className='genre-blues'>
+    Blues
+    <input
+      type="radio"
+      name="genre"
+      value="blues"
+      onChange={handleRadio}
+    />
+  </label>
+  <label className='genre-classical'>
+    Clásica
+    <input
+      type="radio"
+      name="genre"
+      value="classical"
+      onChange={handleRadio}
+    />
+  </label>
+  <label className='genre-samba'>
+    Samba
+    <input
+      type="radio"
+      name="genre"
+      value="samba"
+      onChange={handleRadio}
+    />
+  </label>
+</form>
 }
 
 
@@ -34,13 +82,14 @@ function App() {
   const [recommendation, setRecommendation] = useState(null);
 
   const [genre, setGenre] = useState(null);
-  const [watching, setWatching] = useState('artist')
+  const [artist, setArtist] = useState(null);
+  const [watching, setWatching] = useState('artist');
 
-  const { isLoading } = useQuery(
-    ['songs', genre],
+  const { isLoading: byGenre } = useQuery(
+    ['songsByGenre', genre],
     () => {
       return axios.get(
-        `https://api.spotify.com/v1/recommendations?seed_genres=${genre}&limit=5&target_popularity=10`,
+        `https://api.spotify.com/v1/recommendations?seed_genres=${genre}&limit=5&max_popularity=1`,
         {
           headers: {
             Authorization: `Bearer ${token}`,
@@ -56,11 +105,41 @@ function App() {
           artist: data.tracks[0].artists[0].name,
           name: data.tracks[0].name,
           image: data.tracks[0].album.images[1].url,
+          url: data.tracks[0].external_urls.spotify
         };
 
         setRecommendation(cleanData);
       },
       enabled: !!token && !!genre,
+    }
+  );
+
+  const { isLoading: byArtist } = useQuery(
+    ['songsByArtist', artist],
+    () => {
+      return axios.get(
+        `https://api.spotify.com/v1/recommendations?seed_artists=${artist}&limit=5&max_popularity=13`,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+    },
+    {
+      onSuccess: ({ data }) => {
+        console.log(data, 'de artistas');
+
+        const cleanData = {
+          artist: data.tracks[0].artists[0].name,
+          name: data.tracks[0].name,
+          image: data.tracks[0].album.images[1].url,
+          url: data.tracks[0].external_urls.spotify
+        };
+
+        setRecommendation(cleanData);
+      },
+      enabled: !!token && !!artist,
     }
   );
 
@@ -86,26 +165,17 @@ function App() {
     setToken(token);
   }, []);
 
-  const [trackInfo, setTrackInfo] = useState({
-    artist: '',
-    name: '',
-    genre: '',
-    image: '',
-  });
+  const handleSelect = (ev) => {
+    const value = ev.target.value;
+    console.log(value);
+    setArtist(value)
+  }
 
-
-  const handleSearch = (ev) => {
-    ev.preventDefault();
-    CallToApi(token);
-  };
 
   const handleRadio = (ev) => {
     const value = ev.target.value;
     console.log(value);
-
     setGenre(value);
-
-    // CallToApi(token, value);
   };
 
 
@@ -116,7 +186,7 @@ function App() {
         <a
           href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}
         >
-          Login to Spotify eh
+          Login to Spotify
         </a>
         <nav className='menu'>
           <a className='menu-artist' onClick={() => setWatching('artist')}>Por artista</a>
@@ -124,60 +194,11 @@ function App() {
         </nav>
       </header>
       <main>
-        {watching === 'artist' && <ArtistForm/>}
-        <form className="second-form">
-          <label className="second-form-label">O selecciona un género</label>
-          <label className='genre-pop'>
-            Pop
-            <input
-              type="radio"
-              name="genre"
-              value="pop"
-              onChange={handleRadio}
-            />
-          </label>
-          <label className='genre-indie'>
-            Indie
-            <input
-              type="radio"
-              name="genre"
-              value="indie"
-              onChange={handleRadio}
-            />
-          </label>
-          <label className='genre-blues'>
-            Blues
-            <input
-              type="radio"
-              name="genre"
-              value="blues"
-              onChange={handleRadio}
-            />
-          </label>
-          <label className='genre-classical'>
-            Clásica
-            <input
-              type="radio"
-              name="genre"
-              value="classical"
-              onChange={handleRadio}
-            />
-          </label>
-          <label className='genre-samba'>
-            Samba
-            <input
-              type="radio"
-              name="genre"
-              value="samba"
-              onChange={handleRadio}
-            />
-          </label>
-          {/* <input type="submit" value="Niiice" className="second-form-button" /> */}
-        </form>
-        {/* <button onClick={handleSearch}>Busca</button> */}
-        {genre && isLoading && <p>Carrrrgaannndo que es geruuuuundioooo</p>}
+        {watching === 'artist' && <ArtistForm handleSelect={handleSelect}/>}
+        {watching === 'genre' && <GenreForm handleRadio={handleRadio}/>}
+        {genre && byGenre  && <p>Carrrrgaannndo que es geruuuuundioooo</p>}
 
-        {recommendation && !isLoading && (
+        {recommendation && !byGenre && (
           <div className="recommendation">
             <p>Échale un oído a</p>
             <p>{recommendation.name}</p>
@@ -187,8 +208,24 @@ function App() {
               src={recommendation.image}
               className="recommendation-img"
             />
+            <a href={recommendation.url} target='_blank'>Escúchala aquí</a>
           </div>
         )}
+        {artist && byArtist  && <p>Carrrrgaannndo que es geruuuuundioooo</p>}
+
+        {recommendation && !byArtist && (
+          <div className="recommendation">
+            <p>Échale un oído a</p>
+            <p>{recommendation.name}</p>
+            <p>{recommendation.artist}</p>
+            <img
+              alt={`It's ${recommendation.name} by ${recommendation.artist}`}
+              src={recommendation.image}
+              className="recommendation-img"
+            />
+            <a href={recommendation.url} target='_blank'>Escúchala aquí</a>
+          </div>
+        )}  
       </main>
       <footer className="footer">
         <p>By Ro Flo :)</p>
